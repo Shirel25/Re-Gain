cbe5aa5272fb639c01321413b25e8d5d90cc2404
Erisa Kohansal <erisakohansal@gmail.com>
2025-12-31 20:41:28 +0100

    more obstacles added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 """
 We have two types of surfaces : regular surfaces and the display surface.
@@ -19,11 +20,15 @@ To create enemies:
 3. we move every rectangle in the list to the left on every frame
 4. we remove the rectangles that have moved out of the screen
 """
-
+"""
+Sprite class:
+A class that contains a surface and a rectangle and it can be drawn and updated very easily.
+"""
 
 def main():
 
-    PLAYER_SPEED = 5
+    PLAYER_SPEED = 2
+    OBSTACLE_SPEED = 1
     GROUND_Y = 300
     WIDTH, HEIGHT = 800, 400
 
@@ -33,6 +38,7 @@ def main():
     pygame.display.set_caption("Re:Gain")
     clock = pygame.time.Clock() # limite le FPS et mesure le temps entre frames
     font = pygame.font.Font('Fonts/Pixeltype.ttf', 50)
+    running = True
     game_active = False
     start_time = 0
 
@@ -47,8 +53,11 @@ def main():
     # obstacles
     snail_surface_1 = pygame.image.load('Graphics/Snail/snail1.png').convert_alpha()
     snail_surface_2 = pygame.image.load('Graphics/Snail/snail2.png').convert_alpha()
-    snail = snail_surface_1
-    snail_rect = snail.get_rect(bottomright = (600, 300))
+    fly_surface_1 = pygame.image.load('Graphics/Fly/Fly1.png').convert_alpha()
+    fly_surface_2 = pygame.image.load('Graphics/Fly/Fly2.png').convert_alpha()   
+    obstacle_rect_list = []
+    obstacle_timer = pygame.USEREVENT + 1
+    pygame.time.set_timer(obstacle_timer, 3000)
 
     # player
     player_stand = pygame.image.load('Graphics/Player/player_stand.png').convert_alpha()
@@ -64,13 +73,12 @@ def main():
     replay_instruction_surface = font.render("Press space to replay", False, (111, 196, 169))
     replay_instruction_rect = replay_instruction_surface.get_rect(center = (400, 320))
 
-    while True: # boucle principale du jeu
+    while running: # boucle principale du jeu
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get(): # gestion des événements
             if event.type == pygame.QUIT:
-                game_active = False
-                pygame.quit()
+                running = False
                 break
             
             if game_active:
@@ -85,13 +93,19 @@ def main():
                         player_rect.y += player_gravity
                         player = player_jump
 
+                if event.type == obstacle_timer:
+                    if randint(0, 2): 
+                        obstacle_rect_list.append(snail_surface_1.get_rect(bottomright = (randint(900, 1100), 300)))
+                    else:
+                        obstacle_rect_list.append(fly_surface_1.get_rect(bottomright = (randint(900, 1100), 210)))
+
             else:
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     game_active = True
-                    snail_rect.left = 800
                     player_rect.midbottom = (80, 300)
                     player_gravity = 0  
                     start_time = pygame.time.get_ticks()
+            
         
         if game_active:
             screen.blit(sky_surface, (0,0))
@@ -102,30 +116,48 @@ def main():
             score_rect = score_surface.get_rect(center = (400, 50))
             screen.blit(score_surface, score_rect)
 
-            # snail handling
-            snail_rect.right -= 1
-            if snail_rect.right < 0:
-                snail_rect.left = 805
-            if snail_rect.right % 20 < 10: 
-                snail = snail_surface_1
-            else:
-                snail = snail_surface_2
-            screen.blit(snail, snail_rect)
+            # obstacle handling
+            if obstacle_rect_list:
+                to_delete = []
+                for obstacle_rect in obstacle_rect_list:
+
+                    surface_1 = fly_surface_1
+                    surface_2 = fly_surface_2
+
+                    if obstacle_rect.bottom == 300:
+                        surface_1 = snail_surface_1
+                        surface_2 = snail_surface_2
+
+                    surface = surface_2
+                    obstacle_rect.x -= OBSTACLE_SPEED
+
+                    if obstacle_rect.right < 0:
+                        to_delete.append(obstacle_rect)
+
+                    if obstacle_rect.right % 20 < 10: 
+                        surface = surface_1
+
+                    if player_rect.colliderect(obstacle_rect):
+                        game_active = False
+
+                    screen.blit(surface, obstacle_rect)
+
+                for obj in to_delete:
+                    obstacle_rect_list.remove(obj)
 
 
             # player handling
             keys = pygame.key.get_pressed()
             moving = False
             if keys[pygame.K_RIGHT]:
-                player_rect.right += PLAYER_SPEED
-                if player_rect.right > WIDTH:
-                    player_rect.right = -100
-                moving = True
+                if player_rect.right <= WIDTH:
+                    player_rect.right += PLAYER_SPEED
+                    moving = True
+
             if keys[pygame.K_LEFT]:
-                player_rect.right -= PLAYER_SPEED
-                if player_rect.right < 0:
-                    player_rect.right = WIDTH + 100
-                moving = True
+                if player_rect.left >= 0:
+                    player_rect.right -= PLAYER_SPEED
+                    moving = True
 
             if player_rect.bottom < GROUND_Y:
                 player_gravity += 1
@@ -144,13 +176,6 @@ def main():
                 player = player_stand
 
             screen.blit(player, player_rect)
-
-
-            # mouse_pos = pygame.mouse.get_pos()
-            # collision detection : colliderect and collidepoint
-            if player_rect.colliderect(snail_rect): # no collision 0, there is a collision 1
-                game_active = False
-                # pygame.mouse.get_pressed() to get mouse click
         
         else:
             screen.fill("light blue")
@@ -159,7 +184,10 @@ def main():
             score_surface = font.render(f"Score : {current_score}", False, (111, 196, 169))
             score_rect = score_surface.get_rect(center = (400, 90))
             if current_score > 0 : screen.blit(score_surface, score_rect)
-            screen.blit(replay_instruction_surface, replay_instruction_rect)                
+            screen.blit(replay_instruction_surface, replay_instruction_rect)  
+            obstacle_rect_list.clear()
+            player_rect.midbottom = (80, 300)
+            player_gravity = 0              
 
             
         # flip() the display to put your work on screen
